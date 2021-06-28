73e2b087da9b10076ab8d2c6e93dc1402727ceed
Alexandr Surzhikov <40700736+mansasha21@users.noreply.github.com>
2021-06-28 17:18:58 +0700

    Update download.sh

--- bin/download.sh
+++ bin/download.sh
@@ -1,2 +1,2 @@
 wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mv4AJ3cqe7IpQkFucyWSWeQnLck529nC' -O '../dataset/cats.rar' -r -A 'uc*' -e robots=off -nd
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=11djlIJ1eUfPqBU2uzk7remc9VjEc0t4h' -O '../models/catgen.state' -r -A 'uc*' -e robots=off -nd
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=11tABEiTAhtg6_LyV8Y_ZxwM7Rpy6LQSf' -O '../models/catgen.state' -r -A 'uc*' -e robots=off -nd
